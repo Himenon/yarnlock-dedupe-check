62cfd65ad643383c250c5a398df7fa9acdf38970
Himenon <k.himeno314@gmail.com>
2020-01-27 18:25:23 +0900

    feat: generate install strcture function

--- src/factory.ts
+++ src/factory.ts
@@ -12,22 +12,53 @@ export interface DisplayPackageData {
 }
 
 export interface InstalledStructure {
-  [displayName: string]: DisplayPackageData;
+  [displayName: string]: DisplayPackageData | undefined;
 }
 
-export const generateAllDependencies = (obj: lockfile.YarnLockObject): PackageBasicPairData[] => {
-  const concatPackageAndVersionList = Object.values(obj).reduce<string[]>((all, value) => {
-    return all.concat(Object.entries(value.dependencies || {}).map(entry => `${entry[0]}@${entry[1]}`));
+export const generateUsed = ({ name, version }: PackageBasicPairData, obj: lockfile.YarnLockObject): PackageBasicPairData[] => {
+  return Object.entries(obj).reduce<PackageBasicPairData[]>((used, [packageName, value]) => {
+    // dependenciesに指定したバージョンのライブラリが含まれているかどうか
+    const hasDependency = Object.entries(value.dependencies || {}).find(([depName, depVersion]) => depName === name && depVersion === version);
+    if (hasDependency) {
+      used.push(parseConcatNameAndVersionString(packageName));
+    }
+    return used;
   }, []);
-  return uniq(concatPackageAndVersionList).map(parseConcatNameAndVersionString);
 }
 
-export const generateDisplayPackageData = (obj: lockfile.YarnLockObject) => {
-  const allDependencyPairDataList = generateAllDependencies(obj);
-  const installedPairDataList = Object.keys(obj).map(parseConcatNameAndVersionString);
+export const generateDisplayPackageData = (obj: lockfile.YarnLockObject, checkPackageName: RegExp | undefined = undefined) => {
+  const packageNameList: string[] = [];
+  const dataSet = Object.entries(obj).map(([key, value]) => {
+    const packageBasicPairData = parseConcatNameAndVersionString(key);
+    const ignore: boolean = !!checkPackageName && !packageBasicPairData.name.match(checkPackageName);
+    const used = ignore ? [] : generateUsed(packageBasicPairData, obj);
+    packageNameList.push(packageBasicPairData.name);
+    return {
+      ...packageBasicPairData,
+      realVersion: value.version,
+      used,
+      ignore,
+      rootLibrary: ignore ? "unknown" : used.length === 0,
+    }
+  });
+
+  const installedStructure = uniq(packageNameList).reduce<InstalledStructure>((structure, packageName) => {
+    dataSet.filter(data => data.name === packageName).forEach(d => {
+      const newDisplayPackageData: DisplayPackageData = {
+        [d.version]: {
+          realUsedVersion: d.realVersion,
+          used: d.used,
+        }
+      }
+      structure[packageName] = {
+        ...(structure[packageName] || {}),
+        ...newDisplayPackageData,
+      };;
+    })
+    return structure;
+  }, {});
 
   return {
-    allDependencyPairDataList,
-    installedPairDataList,
+    installedStructure
   };
 }
